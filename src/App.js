8b58bce39588c5283d11d8b4bcae4851c65cff2e
Mariusz Sternak <aquzif@gmail.com>
2023-06-24 21:57:36 +0200

    fix cicd error

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload. <br/>
-          Test CICD
 
         </p>
         <a
