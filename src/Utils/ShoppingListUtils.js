50d5c3d34d9d1f82dfe03d90b9da5cd776211dac
Mariusz Sternak <aquzif@gmail.com>
2023-12-14 18:22:35 +0100

    Add sorting to shopping list

---
+++ src/Utils/ShoppingListUtils.js
@@ -0,0 +1,70 @@
+import {ray} from "node-ray/web";
+import ArrayUtils from "@/Utils/ArrayUtils";
+
+export default class ShoppingListUtils {
+
+    static sortAndPrepareShoppingList(shoppingList) {
+
+        let shoppingListToReturn = [];
+
+        if(!shoppingList) return [];
+
+        //ray clear session
+        ray().clearScreen();
+
+        let {type, entries} = shoppingList;
+        if(type === 'default'){
+            shoppingListToReturn = [
+                ...entries.filter(entry => !entry.checked).map(entry => {
+                    return {
+                        type: 'entry',
+                        ...entry
+                    }
+                }),
+                ...entries.filter(entry => entry.checked).map(entry => {
+                    return {
+                        type: 'entry',
+                        ...entry
+                    }
+                })
+            ];
+        } else if(type === 'grouped'){
+
+            Object.entries(ArrayUtils.
+            groupBy(entries
+                .filter(entry => !entry.checked),'category'))
+                .map(([key,entries]) => {
+                    shoppingListToReturn = [...shoppingListToReturn,
+                        {
+                            type: 'category',
+                            category: key
+                        },
+                        ...entries.map(entry => {
+                            return {
+                                type: 'entry',
+                                ...entry
+                            }
+                        })
+                    ];
+                });
+            if(entries.filter(entry => entry.checked).length > 0){
+                shoppingListToReturn = [...shoppingListToReturn,
+                    {
+                        type: 'category',
+                        category: 'checked'
+                    },
+                    ...entries.filter(entry => entry.checked).map(entry => {
+                        return {
+                            type: 'entry',
+                            ...entry
+                        }
+                    })
+                ];
+            }
+        }
+
+        return shoppingListToReturn;
+
+    }
+
+}
